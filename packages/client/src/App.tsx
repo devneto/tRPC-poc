05489eae1cb027d77afb9a733cb9c87bbcbcc775
devneto <miguel.neto320@gmail.com>
2022-07-27 23:10:46 -0300

    feat: client setup query

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
+import { trpc } from "./trpc";
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: client</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+const client = new QueryClient();
+
+const AppContent = () => {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: client</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Tailwind</div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [ trpcClient ] = useState(() => trpc.createClient({
+    url: 'http://localhost:8000/trpc'
+  }))
+  return (
+    <trpc.Provider client={trpcClient} queryClient={client}>
+      <QueryClientProvider client={client}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+ReactDOM.render(<AppContent />, document.getElementById("app"));
